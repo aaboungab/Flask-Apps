60d9dc8ced653068b4ade511b9d9b04d402bbeec
aaboungab <72809072+aaboungab@users.noreply.github.com>
2020-11-03 14:24:05 +0000

    Update app.py

--- DBLayer_relationships/app.py
+++ DBLayer_relationships/app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__) 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:11@34.89.108.42/DBL_relationship' 
+app.config['SQLALCHEMY_DATABASE_URI'] = '' 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
 db = SQLAlchemy(app) 
 
