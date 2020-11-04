af7644a56395db973bb467a7cd84978abd1247f1
anas aboungab <aaboungab@qa.com>
2020-11-04 15:26:10 +0000

    QA DBLayer_relationship exercise

--- M2M_exercise/app.py
+++ M2M_exercise/app.py
@@ -1,23 +1,28 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:11@34.89.108.42/M2M_exercise'
 app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
-
-class Customers(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	first_name = db.Column(db.String(30), nullable=False)
-	last_name = db.Column(db.String(30), nullable=False)
+db = SQLAlchemy(app)
 
 class Orders(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	product_id = db.Column(db.Integer, db.ForeignKey('products.id'), nullable=False)
-	customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    product_id = db.Column(db.Integer, db.ForeignKey('products.id'), nullable=False)
+    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'), nullable=False)
 
 class Products(db.Model):
-	id = db.Column(db.integer, primary_key=True)
-	name = db.Column(db.String(30), nullable=False)
-	price = db.Column(db.Float, nullable=False)
-	orders = db.relationship('Orders', backref='product')
-	
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    orders = db.relationship('Orders', backref='product')
+
+class Customers(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String(30), nullable=False)
+    email = db.Column(db.String(30), nullable=False, unique=True)
+    orders = db.relationship('Orders', backref='customer')
+
+if __name__ == '__main__':
+     app.run(debug=True, host='0.0.0.0')
--- M2M_exercise/create.py
+++ M2M_exercise/create.py
@@ -1,12 +1,15 @@
 from app import db, Orders, Customers, Products 
 
-db.Create_all()
+db.create_all()
 
-ben = Customers(first_name='Ben')
-phone = Products()
+anas = Customers(full_name='Anas Aboungab', email='aaboungab@gcp.com')
+PS5 = Products(name='Playstation 5', price=499.99)
 
-db.session.add(ben)
-db.session.add(phone)
+db.session.add(anas)
+db.session.add(PS5)
 db.session.commit()
 
-order_phone = Orders(proudct_id=phone.id, customer_id=customers.id)
+order_PS5 = Orders(proudct_id=PS5.id, customer_id=anas.id)
+
+db.session.add(order_PS5)
+db.session.commit()
