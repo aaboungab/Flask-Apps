2db96ab7303deeb9b21511f767ebafa67644c9c7
anas aboungab <aaboungab@qa.com>
2020-11-04 16:25:47 +0000

    ToDo-List_exercise

--- ToDoList_ex/application/routes.py
+++ ToDoList_ex/application/routes.py
@@ -1,34 +1,47 @@
+from flask import render_template, redirect, url_for
+
 from application import app,db
 from application.models import ToDoList
 
 @app.route('/')
-def home():
-	all_ToDo = ToDoList.query.all()
-	all_ToDo_list=""
-	for ToDo in all_ToDo:
-		all_ToDo_list+="<br>" + ToDo.name
-	return all_ToDo_list
+def index():
+    all_ToDo = ToDoList.query.all()
+    return render_template('index.html', all_ToDo=all_ToDo)
 
 
-@app.route('/add/<newToDo>')
-def add(newToDo):
-	new_ToDo = ToDoList(name=newToDo)
-	db.session.add(new_ToDo)
-	db.session.commit()
-	return "Added new ToDo added into list"
+@app.route('/add')
+def add():
+    new_ToDo = ToDoList(name='New ToDo')
+    db.session.add(new_ToDo)
+    db.session.commit()
+    return redirect(url_for('index'))
 
-@app.route('/complete')
-def complete():
+@app.route('/complete/<int:ToDo_id>')
+def complete(ToDo_id):
+    ToDo_to_update = ToDoList.query.get(ToDo_id)
+    ToDo_to_update.complete = True
+    db.session.commit()
+    return redirect(url_for('index'))
 
-@app.route('/incomplete')
-def incomplete():
+@app.route('/incomplete/<int:ToDo_id>')
+def incomplete(ToDo_id):
+    ToDo_to_update = ToDoList.query.get(ToDo_id)
+    ToDo_to_update.complete = False
+    db.session.commit()
+    return redirect(url_for('index'))
 
-@app.route('update')
-def update():
-	first_ToDo = ToDoList.query.first()
-	
-	db.session.commit()
-	return  
 
-@app.route('/delete')
-def delete():
+@app.route('/update/<task>')
+def update(task):
+    ToDo_to_update = ToDoList.query.first()
+    ToDo_to_update.task = task
+    db.session.commit()
+    return redirect(url_for('index')) 
+
+@app.route('/delete/<int:ToDo_id>')
+def delete(ToDo_id):
+    ToDo_to_delete = ToDoList.query.get(ToDo_id)
+    db.session.delete(ToDo_to_delete)
+    db.session.commit()
+    return redirect(url_for('index'))
+
